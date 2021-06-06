72f4e1671e8f0fde39facdfa7e7d55af79c9820d
Shyam <40246847+ShyamKumar1@users.noreply.github.com>
2021-06-06 09:53:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>main.py</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<style type="text/css">
+.s0 { color: #cc7832;}
+.s1 { color: #a9b7c6;}
+.s2 { color: #6a8759;}
+.s3 { color: #6897bb;}
+.s4 { color: #808080;}
+</style>
+</head>
+<body bgcolor="#2b2b2b">
+<table CELLSPACING=0 CELLPADDING=5 COLS=1 WIDTH="100%" BGCOLOR="#606060" >
+<tr><td><center>
+<font face="Arial, Helvetica" color="#000000">
+main.py</font>
+</center></td></tr></table>
+<pre><span class="s0">import </span><span class="s1">pywhatkit </span><span class="s0">as </span><span class="s1">pwk</span>
+
+<span class="s1">pwk.sendwhatmsg(</span><span class="s2">'+911234567890'</span><span class="s0">, </span><span class="s2">'Sample Message'</span><span class="s0">, </span><span class="s3">8</span><span class="s0">, </span><span class="s3">23</span><span class="s1">)  </span>
+<span class="s4"># Syntax: pywhatkit.sendwhatmsg('phone.no string', 'message String', hour int, minutes int)</span>
+</pre>
+</body>
+</html>
